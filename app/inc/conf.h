dd19225284d2b63e4d8c6ab64a0b9448458370d8
Alopex6414 <1029535012@qq.com>
2025-06-28 10:49:41 +0800

    set K64 PLL Core CLK to 120MHz

--- app/inc/conf.h
+++ app/inc/conf.h
@@ -23,19 +23,19 @@
 /*
 **PLL锁相环频率设置宏
 */
-#define MK64_CORE_PLL_CLK       PLL100          //内核PLL频率(PLL150-PLL180比较稳定,建议PLL150)
+#define MK64_CORE_PLL_CLK       PLL120          //内核PLL频率(PLL150-PLL180比较稳定,建议PLL150)
 
-#define MK64_CORE_CLK           100             //内核时钟频率
-#define MK64_BUS_CLK             50             //总线时钟频率
-#define MK64_FLEX_CLK            25             //FLEX时钟频率
-#define MK64_FLASH_CLK           25             //FLASH时钟频率
+#define MK64_CORE_CLK           120             //内核时钟频率
+#define MK64_BUS_CLK             60             //总线时钟频率
+#define MK64_FLEX_CLK            30             //FLEX时钟频率
+#define MK64_FLASH_CLK           30             //FLASH时钟频率
 /*
 **SIM分频因子设置
 */
 #define MK64_CORE_DIV   0
 #define MK64_BUS_DIV    1
-#define MK64_FLEX_DIV   7
-#define MK64_FLASH_DIV  7
+#define MK64_FLEX_DIV   3
+#define MK64_FLASH_DIV  3
 
 /*
 **DWT延时
