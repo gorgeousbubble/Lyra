7af7ede87ca5fba307a214f8f870d6462c7398d3
Alopex6414 <1029535012@qq.com>
2026-02-18 21:30:25 +0800

    format filter header

--- app/inc/filter.h
+++ app/inc/filter.h
@@ -32,18 +32,21 @@ typedef struct
 
 // Complementary filter structure
 // Attitude structure
-typedef struct {
-    float roll;      // X-axis rotation
-    float pitch;     // Y-axis rotation
-    float yaw;       // Z-axis rotation
+typedef struct
+{
+    float roll;  // X-axis rotation
+    float pitch; // Y-axis rotation
+    float yaw;   // Z-axis rotation
 } Attitude_t;
 
-typedef struct {
-    float q0, q1, q2, q3;  // Quaternion [w, x, y, z]
+typedef struct
+{
+    float q0, q1, q2, q3; // Quaternion [w, x, y, z]
 } Quaternion_t;
 
 // Complementary filter structure
-typedef struct {
+typedef struct
+{
     Attitude_t angle;
     Quaternion_t quat;
     float kp;
@@ -64,12 +67,12 @@ typedef struct {
 */
 extern void Kalman_Init(KalmanFilter *kf, float p[2][2], float dt, float q_angle, float q_gyro, float r_angle);
 extern float Kalman_Filter(KalmanFilter *kf, float angle_m, float gyro_m);
-extern void Complementary_Filter_Init(ComplementaryFilter* filter, float kp, float ki, float dt);
-extern void Complementary_Filter_Update(ComplementaryFilter* filter, float ax, float ay, float az, float gx, float gy, float gz);
-extern void Convert_Sensor_Data(float* accel, float* gyro, const int acc_raw[3], const int gyro_raw[3], float acc_range, float gyro_range);
-extern void Calculate_Euler_Angles(ComplementaryFilter* filter);
+extern void Complementary_Filter_Init(ComplementaryFilter *filter, float kp, float ki, float dt);
+extern void Complementary_Filter_Update(ComplementaryFilter *filter, float ax, float ay, float az, float gx, float gy, float gz);
+extern void Convert_Sensor_Data(float *accel, float *gyro, const int acc_raw[3], const int gyro_raw[3], float acc_range, float gyro_range);
+extern void Calculate_Euler_Angles(ComplementaryFilter *filter);
 extern float Normalize_Angle(float angle);
-extern void Get_Attitude_Angles(ComplementaryFilter* filter, float *roll, float *pitch, float *yaw);
-extern void Get_Quaternion(ComplementaryFilter* filter, float *q0, float *q1, float *q2, float *q3);
+extern void Get_Attitude_Angles(ComplementaryFilter *filter, float *roll, float *pitch, float *yaw);
+extern void Get_Quaternion(ComplementaryFilter *filter, float *q0, float *q1, float *q2, float *q3);
 
 #endif
