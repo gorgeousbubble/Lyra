244aee4740acf34f9c8f761fe96067c78ad25aea
Alopex6414 <1029535012@qq.com>
2026-02-19 12:16:50 +0800

    format document

--- board/inc/maps_dock_key.h
+++ board/inc/maps_dock_key.h
@@ -60,8 +60,8 @@ extern uint8 MAPS_Dock_KEY_KEYn_Get(MAPS_Dock_KEYn MAPS_Dock_KEYx);
 extern MAPS_Dock_KEY_Status MAPS_Dock_KEY_KEYn_Check(MAPS_Dock_KEYn MAPS_Dock_KEYx);
 extern void MAPS_Dock_KEY_Incident(void);
 
-extern void Calc_Dynamic_Animation_Cache_Array(Coord** buff, int* buffSize, const uint8 array[64][16]);
-extern void Release_Dynamic_Animation_Cache(CoordCache* array, int len);
-extern void Refresh_Dynamic_Animation_Cache(CoordCache* array, int len, int menu, int index[2]);
+extern void Calc_Dynamic_Animation_Cache_Array(Coord **buff, int *buffSize, const uint8 array[64][16]);
+extern void Release_Dynamic_Animation_Cache(CoordCache *array, int len);
+extern void Refresh_Dynamic_Animation_Cache(CoordCache *array, int len, int menu, int index[2]);
 
 #endif
