4e93a1594d101653e66f89a005cb5ab735bc528a
Alopex6414 <1029535012@qq.com>
2026-02-26 22:39:29 +0800

    format spi.h

--- chip/inc/spi.h
+++ chip/inc/spi.h
@@ -24,7 +24,7 @@ typedef enum
   SPI_SPI1,
   SPI_SPI2,
   SPI_SPI_MAX,
-}SPI_SPIn;
+} SPI_SPIn;
 
 /*
 **SPI mode enumeration
@@ -33,7 +33,7 @@ typedef enum
 {
   MASTER,
   SLAVE,
-}SPI_CFG;
+} SPI_CFG;
 
 /*
 **SPI module chip selection enumeration
@@ -46,14 +46,14 @@ typedef enum
   SPI_PCS3 = 1 << 3,
   SPI_PCS4 = 1 << 4,
   SPI_PCS5 = 1 << 5,
-}SPI_PCSn;
+} SPI_PCSn;
 
 /*
 **SPI macro
 */
-#define SPI_TX_WAIT(SPIn)       while((SPI_SR_REG(SPIN[SPIn]) & SPI_SR_TXRXS_MASK) == 1)
-#define SPI_RX_WAIT(SPIn)       while((SPI_SR_REG(SPIN[SPIn]) & SPI_SR_RFDF_MASK) == 0)
-#define SPI_EOQF_WAIT(SPIn)     while((SPI_SR_REG(SPIN[SPIn]) & SPI_SR_EOQF_MASK) == 0)
+#define SPI_TX_WAIT(SPIn) while ((SPI_SR_REG(SPIN[SPIn]) & SPI_SR_TXRXS_MASK) == 1)
+#define SPI_RX_WAIT(SPIn) while ((SPI_SR_REG(SPIN[SPIn]) & SPI_SR_RFDF_MASK) == 0)
+#define SPI_EOQF_WAIT(SPIn) while ((SPI_SR_REG(SPIN[SPIn]) & SPI_SR_EOQF_MASK) == 0)
 
 /*
 **variate declaration
@@ -63,10 +63,10 @@ extern SPI_MemMapPtr SPIN[SPI_SPI_MAX];
 /*
 **function declaration
 */
-extern uint32 SPI_Init(SPI_SPIn SPI_SPIx,SPI_PCSn SPI_PCSx,SPI_CFG SPI_cfg,uint32 SPI_Baud);
-extern void SPI_Extra_PCS_Init(SPI_SPIn SPI_SPIx,SPI_PCSn SPI_PCSx);
-extern void SPI_MOSI(SPI_SPIn SPI_SPIx,SPI_PCSn SPI_PCSx,uint8 *SPI_MO_Data,uint8 *SPI_MI_Data,uint32 SPI_Len);
-extern void SPI_MOSI_CMD(SPI_SPIn SPI_SPIx,SPI_PCSn SPI_PCSx,uint8 *SPI_MO_CMD,uint8 *SPI_MI_CMD,uint8 *SPI_MO_Data,uint8 *SPI_MI_Data,uint32 SPI_CMD_Len,uint32 SPI_Len);
-extern void SPI_Send(SPI_SPIn SPI_SPIx,SPI_PCSn SPI_PCSx,uint8 Data);
+extern uint32 SPI_Init(SPI_SPIn SPI_SPIx, SPI_PCSn SPI_PCSx, SPI_CFG SPI_cfg, uint32 SPI_Baud);
+extern void SPI_Extra_PCS_Init(SPI_SPIn SPI_SPIx, SPI_PCSn SPI_PCSx);
+extern void SPI_MOSI(SPI_SPIn SPI_SPIx, SPI_PCSn SPI_PCSx, uint8 *SPI_MO_Data, uint8 *SPI_MI_Data, uint32 SPI_Len);
+extern void SPI_MOSI_CMD(SPI_SPIn SPI_SPIx, SPI_PCSn SPI_PCSx, uint8 *SPI_MO_CMD, uint8 *SPI_MI_CMD, uint8 *SPI_MO_Data, uint8 *SPI_MI_Data, uint32 SPI_CMD_Len, uint32 SPI_Len);
+extern void SPI_Send(SPI_SPIn SPI_SPIx, SPI_PCSn SPI_PCSx, uint8 Data);
 
 #endif
