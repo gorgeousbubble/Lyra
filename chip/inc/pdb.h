0ee3e058c176bf12080dcc3cf6f079a20054398c
Alopex6414 <1029535012@qq.com>
2026-02-26 22:28:08 +0800

    format pdb.h

--- chip/inc/pdb.h
+++ chip/inc/pdb.h
@@ -20,23 +20,22 @@
 */
 typedef enum
 {
-  PDB_External_Trigger,         //External trigger source
-  PDB_CMP0_Trigger,             //External trigger CMP0
-  PDB_CMP1_Trigger,             //External trigger CMP1
-  PDB_CMP2_Trigger,             //External trigger CMP2
-  PDB_PIT0_Trigger,             //External trigger PIT0
-  PDB_PIT1_Trigger,             //External trigger PIT1
-  PDB_PIT2_Trigger,             //External trigger PIT2
-  PDB_PIT3_Trigger,             //External trigger PIT3
-  PDB_FTM0_Trigger,             //External trigger FTM0
-  PDB_FTM1_Trigger,             //External trigger FTM1
-  PDB_FTM2_Trigger,             //External trigger FTM2
-  PDB_FTM3_Trigger,             //External trigger FTM3
-  PDB_RTC_Alarm_Trigger,        //External trigger RTC_Alarm
-  PDB_RTC_Seconds_Trigger,      //External trigger RTC_Seconds
-  PDB_LPTMR_Trigger,            //External trigger LMPTR
-  PDB_Software_Trigger,         //Software trigger
-}PDB_Trigger_Source;
-
+  PDB_External_Trigger,    // External trigger source
+  PDB_CMP0_Trigger,        // External trigger CMP0
+  PDB_CMP1_Trigger,        // External trigger CMP1
+  PDB_CMP2_Trigger,        // External trigger CMP2
+  PDB_PIT0_Trigger,        // External trigger PIT0
+  PDB_PIT1_Trigger,        // External trigger PIT1
+  PDB_PIT2_Trigger,        // External trigger PIT2
+  PDB_PIT3_Trigger,        // External trigger PIT3
+  PDB_FTM0_Trigger,        // External trigger FTM0
+  PDB_FTM1_Trigger,        // External trigger FTM1
+  PDB_FTM2_Trigger,        // External trigger FTM2
+  PDB_FTM3_Trigger,        // External trigger FTM3
+  PDB_RTC_Alarm_Trigger,   // External trigger RTC_Alarm
+  PDB_RTC_Seconds_Trigger, // External trigger RTC_Seconds
+  PDB_LPTMR_Trigger,       // External trigger LMPTR
+  PDB_Software_Trigger,    // Software trigger
+} PDB_Trigger_Source;
 
 #endif
