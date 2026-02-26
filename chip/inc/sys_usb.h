fd38a311b24c249e3693d95e61f4d87fb534108b
Alopex6414 <1029535012@qq.com>
2026-02-26 22:41:17 +0800

    format sys_usb.h

--- chip/inc/sys_usb.h
+++ chip/inc/sys_usb.h
@@ -15,10 +15,10 @@
 
 #include "common.h"
 
-//USB configuration related macros
-#define USB_CLOCK_PLL                   //USB enable PLL clock
-#define USB_CLK_DIV             4       //USB clock source frequency division factor
-#define USB_CLK_FRAC            1       //USB clock source multiplication factor
+// USB configuration related macros
+#define USB_CLOCK_PLL  // USB enable PLL clock
+#define USB_CLK_DIV 4  // USB clock source frequency division factor
+#define USB_CLK_FRAC 1 // USB clock source multiplication factor
 
 /*
 **function declaration
@@ -27,6 +27,6 @@ extern void USB_Init(void);
 extern void USB_COM_Init(void);
 extern void USB_Enum_Wait(void);
 extern uint8 USB_COM_Rx(uint8_t *Rx_Buf);
-extern void USB_COM_Tx(uint8 *Tx_Buf,uint8 Len);
+extern void USB_COM_Tx(uint8 *Tx_Buf, uint8 Len);
 
 #endif
