f086502da9b47fcadbe332ec23a80e63da87d3b8
Alopex6414 <1029535012@qq.com>
2026-02-26 23:08:06 +0800

    format pit.c

--- chip/src/pit.c
+++ chip/src/pit.c
@@ -23,20 +23,19 @@
                 Set_Vector_Handler(PIT0_VECTORn,pit_hander);   //Set the interrupt service function to the interrupt vector table
                 enable_irq(PIT0_IRQn);                         //Enable PIT interrupt
  */
-void PIT_Init(PITn PITx,uint32 PIT_Count)
+void PIT_Init(PITn PITx, uint32 PIT_Count)
 {
-  //Assert PIT module number, PIT timing value is greater than 0
+  // Assert PIT module number, PIT timing value is greater than 0
   ASSERT((PITx <= PIT3) && (PIT_Count > 0));
-  
-  SIM_SCGC6 |= SIM_SCGC6_PIT_MASK;//PIT clock enable
-  
-  PIT_MCR &= ~(PIT_MCR_MDIS_MASK | PIT_MCR_FRZ_MASK);//PIT runs in debugging mode with PIT enabled
-  PIT_LDVAL(PITx) = PIT_Count - 1;//Load PIT timer initial value (countdown)
-  
-  PIT_Flag_Clear(PITx);//Clear interrupt flag
-  
-  PIT_TCTRL(PITx) = (0 | PIT_TCTRL_TIE_MASK | PIT_TCTRL_TEN_MASK);//Timer interrupt enable, timer enable, without using counter cascading
-  
+
+  SIM_SCGC6 |= SIM_SCGC6_PIT_MASK; // PIT clock enable
+
+  PIT_MCR &= ~(PIT_MCR_MDIS_MASK | PIT_MCR_FRZ_MASK); // PIT runs in debugging mode with PIT enabled
+  PIT_LDVAL(PITx) = PIT_Count - 1;                    // Load PIT timer initial value (countdown)
+
+  PIT_Flag_Clear(PITx); // Clear interrupt flag
+
+  PIT_TCTRL(PITx) = (0 | PIT_TCTRL_TIE_MASK | PIT_TCTRL_TEN_MASK); // Timer interrupt enable, timer enable, without using counter cascading
 }
 
 /*
@@ -46,28 +45,27 @@ void PIT_Init(PITn PITx,uint32 PIT_Count)
  *  @since      v1.0
  *  Sample usage:       PIT_Delay(PIT0, 1000);  //delay 1000 bus clock
  */
-void PIT_Delay(PITn PITx,uint32 PIT_Count)
+void PIT_Delay(PITn PITx, uint32 PIT_Count)
 {
-  //Assert PIT module number, PIT timing value is greater than 0
+  // Assert PIT module number, PIT timing value is greater than 0
   ASSERT((PITx <= PIT3) && (PIT_Count > 0));
-  
-  SIM_SCGC6 |= SIM_SCGC6_PIT_MASK;//PIT clock enable
-  
-  PIT_MCR &= ~(PIT_MCR_MDIS_MASK | PIT_MCR_FRZ_MASK);//PIT runs in debugging mode with PIT enabled
-  PIT_TCTRL(PITx) &= ~PIT_TCTRL_TEN_MASK;//Prohibit PIT interruption
-  PIT_LDVAL(PITx) = PIT_Count - 1;//Load PIT timer initial value (countdown)
-  
-  PIT_Flag_Clear(PITx);//Clear interrupt flag
-  
-  PIT_TCTRL(PITx) &= ~PIT_TCTRL_TEN_MASK;//Prohibit PIT interruption
-  
-  
-  PIT_TCTRL(PITx) = (0 | PIT_TCTRL_TEN_MASK);//Timer enabled, without cascading counters
-  
-  while((PIT_TFLG(PITx) & PIT_TFLG_TIF_MASK) != PIT_TFLG_TIF_MASK);
-  
-  PIT_Flag_Clear(PITx);//Clear interrupt flag
-  
+
+  SIM_SCGC6 |= SIM_SCGC6_PIT_MASK; // PIT clock enable
+
+  PIT_MCR &= ~(PIT_MCR_MDIS_MASK | PIT_MCR_FRZ_MASK); // PIT runs in debugging mode with PIT enabled
+  PIT_TCTRL(PITx) &= ~PIT_TCTRL_TEN_MASK;             // Prohibit PIT interruption
+  PIT_LDVAL(PITx) = PIT_Count - 1;                    // Load PIT timer initial value (countdown)
+
+  PIT_Flag_Clear(PITx); // Clear interrupt flag
+
+  PIT_TCTRL(PITx) &= ~PIT_TCTRL_TEN_MASK; // Prohibit PIT interruption
+
+  PIT_TCTRL(PITx) = (0 | PIT_TCTRL_TEN_MASK); // Timer enabled, without cascading counters
+
+  while ((PIT_TFLG(PITx) & PIT_TFLG_TIF_MASK) != PIT_TFLG_TIF_MASK)
+    ;
+
+  PIT_Flag_Clear(PITx); // Clear interrupt flag
 }
 
 /*
@@ -78,20 +76,19 @@ void PIT_Delay(PITn PITx,uint32 PIT_Count)
  */
 void PIT_Time_Start(PITn PITx)
 {
-  //Assert PIT module number
+  // Assert PIT module number
   ASSERT(PITx <= PIT3);
-  
-  SIM_SCGC6 |= SIM_SCGC6_PIT_MASK;//PIT clock enable
-  
-  PIT_MCR &= ~(PIT_MCR_MDIS_MASK | PIT_MCR_FRZ_MASK);//PIT runs in debugging mode with PIT enabled
-  PIT_TCTRL(PITx) &= ~PIT_TCTRL_TEN_MASK;//Prohibit PIT interruption
-  PIT_LDVAL(PITx) = ~0;//Load PIT timer initial value (countdown)
-  
-  PIT_Flag_Clear(PITx);//Clear interrupt flag
-  
-  PIT_TCTRL(PITx) &= ~PIT_TCTRL_TEN_MASK;//Prohibit PIT interruption
-  PIT_TCTRL(PITx) = (0 | PIT_TCTRL_TEN_MASK);//Timer enabled, without cascading counters
-  
+
+  SIM_SCGC6 |= SIM_SCGC6_PIT_MASK; // PIT clock enable
+
+  PIT_MCR &= ~(PIT_MCR_MDIS_MASK | PIT_MCR_FRZ_MASK); // PIT runs in debugging mode with PIT enabled
+  PIT_TCTRL(PITx) &= ~PIT_TCTRL_TEN_MASK;             // Prohibit PIT interruption
+  PIT_LDVAL(PITx) = ~0;                               // Load PIT timer initial value (countdown)
+
+  PIT_Flag_Clear(PITx); // Clear interrupt flag
+
+  PIT_TCTRL(PITx) &= ~PIT_TCTRL_TEN_MASK;     // Prohibit PIT interruption
+  PIT_TCTRL(PITx) = (0 | PIT_TCTRL_TEN_MASK); // Timer enabled, without cascading counters
 }
 
 /*
@@ -102,27 +99,26 @@ void PIT_Time_Start(PITn PITx)
  */
 uint32 PIT_Time_Get(PITn PITx)
 {
-  uint32 PIT_Value=0;
-  
-  //Assert PIT module number
+  uint32 PIT_Value = 0;
+
+  // Assert PIT module number
   ASSERT(PITx <= PIT3);
-  
-  PIT_Value = ~0 - PIT_CVAL(PITx);//Count time value=initial count value 0~- current count value PIT_CVAL
-  
-  if(PIT_TFLG(PITx) & PIT_TFLG_TIF_MASK)//Has it exceeded the counting time
+
+  PIT_Value = ~0 - PIT_CVAL(PITx); // Count time value=initial count value 0~- current count value PIT_CVAL
+
+  if (PIT_TFLG(PITx) & PIT_TFLG_TIF_MASK) // Has it exceeded the counting time
   {
-    PIT_Flag_Clear(PITx);//Clear overflow flag
-    PIT_TCTRL(PITx) &= ~PIT_TCTRL_TEN_MASK;//Prohibit PIT interruption
+    PIT_Flag_Clear(PITx);                   // Clear overflow flag
+    PIT_TCTRL(PITx) &= ~PIT_TCTRL_TEN_MASK; // Prohibit PIT interruption
     return ~0;
   }
-  
-  if(PIT_Value == ~0)
+
+  if (PIT_Value == ~0)
   {
     PIT_Value = 0xffffffff - 0x1;
   }
-  
+
   return PIT_Value;
-  
 }
 
 /*
@@ -133,10 +129,9 @@ uint32 PIT_Time_Get(PITn PITx)
  */
 void PIT_Close(PITn PITx)
 {
-  //Assert PIT module number
+  // Assert PIT module number
   ASSERT(PITx <= PIT3);
-  
-  PIT_Flag_Clear(PITx);//Clear overflow flag
-  PIT_TCTRL(PITx) &= ~PIT_TCTRL_TEN_MASK;//Prohibit PIT interruption
-  
+
+  PIT_Flag_Clear(PITx);                   // Clear overflow flag
+  PIT_TCTRL(PITx) &= ~PIT_TCTRL_TEN_MASK; // Prohibit PIT interruption
 }
