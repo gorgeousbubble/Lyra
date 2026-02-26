ccf9a6f92280d80ec1685351cbe8577d0474136d
Alopex6414 <1029535012@qq.com>
2026-02-26 23:09:02 +0800

    format rtc.c

--- chip/src/rtc.c
+++ chip/src/rtc.c
@@ -20,42 +20,41 @@
  */
 void RTC_Init(void)
 {
-  volatile uint32 RTC_Delay=0;
-  volatile uint32 TSR=0;
-  volatile uint32 TAR=0;
-  
-  SIM_SCGC6 |= SIM_SCGC6_RTC_MASK;//RTC clock enable
+  volatile uint32 RTC_Delay = 0;
+  volatile uint32 TSR = 0;
+  volatile uint32 TAR = 0;
+
+  SIM_SCGC6 |= SIM_SCGC6_RTC_MASK; // RTC clock enable
   PMC->REGSC |= PMC_REGSC_BGBE_MASK;
-  
+
   TSR = RTC_TSR;
   TAR = RTC_TAR;
-  
-  RTC_CR = RTC_CR_SWR_MASK;     //RTC register reset
-  RTC_CR &= ~RTC_CR_SWR_MASK;   //RTC software reset
-  
-  RTC_CR =(0
-           | RTC_CR_OSCE_MASK   //Enable 32.768KHz crystal oscillator
-           | RTC_CR_SC16P_MASK  //Enable 16pF bypass capacitor
-           | RTC_CR_CLKO_MASK   //32.768KHz wave does not output to the outside
-           );
-  
-  //Waiting for the crystal oscillator to stabilize
+
+  RTC_CR = RTC_CR_SWR_MASK;   // RTC register reset
+  RTC_CR &= ~RTC_CR_SWR_MASK; // RTC software reset
+
+  RTC_CR = (0 | RTC_CR_OSCE_MASK // Enable 32.768KHz crystal oscillator
+            | RTC_CR_SC16P_MASK  // Enable 16pF bypass capacitor
+            | RTC_CR_CLKO_MASK   // 32.768KHz wave does not output to the outside
+  );
+
+  // Waiting for the crystal oscillator to stabilize
   RTC_Delay = 0x600000;
-  while(RTC_Delay--);
-  
-  //Set time compensation
-  RTC_TCR = (0
-             | RTC_TCR_CIR(0)   //Crystal oscillator compensation time is 0
-             | RTC_TCR_TCR(0)   //32.768KHz crystal oscillator time compensation (uncompensated)
-             );
-  
-  RTC_SR &= ~RTC_SR_TCE_MASK;   //Disable RTC counter 
-  //Time and alarm settings
-  RTC_TSR = TSR;                  //The initial value of the second counter is 0
-  RTC_TAR = TAR;                  //Alarm clock counter value 0
-  RTC_IER = 0;                  //Disable interrupt
-  
-  RTC_SR |= RTC_SR_TCE_MASK;    //Enable RTC counter
+  while (RTC_Delay--)
+    ;
+
+  // Set time compensation
+  RTC_TCR = (0 | RTC_TCR_CIR(0) // Crystal oscillator compensation time is 0
+             | RTC_TCR_TCR(0)   // 32.768KHz crystal oscillator time compensation (uncompensated)
+  );
+
+  RTC_SR &= ~RTC_SR_TCE_MASK; // Disable RTC counter
+  // Time and alarm settings
+  RTC_TSR = TSR; // The initial value of the second counter is 0
+  RTC_TAR = TAR; // Alarm clock counter value 0
+  RTC_IER = 0;   // Disable interrupt
+
+  RTC_SR |= RTC_SR_TCE_MASK; // Enable RTC counter
 }
 
 /*
@@ -64,9 +63,9 @@ void RTC_Init(void)
  */
 void RTC_Set_Time(uint32 Seconds)
 {
-  RTC_SR &= ~RTC_SR_TCE_MASK;   //Disable RTC counter
-  RTC_TSR = Seconds;            //Set the initial value time of the counter
-  RTC_SR |= RTC_SR_TCE_MASK;    //Enable RTC counter
+  RTC_SR &= ~RTC_SR_TCE_MASK; // Disable RTC counter
+  RTC_TSR = Seconds;          // Set the initial value time of the counter
+  RTC_SR |= RTC_SR_TCE_MASK;  // Enable RTC counter
 }
 
 /*
@@ -75,11 +74,11 @@ void RTC_Set_Time(uint32 Seconds)
  */
 void RTC_Set_Time_Format(struct tm *timeinfo)
 {
-  //Convert struct tm to seconds
+  // Convert struct tm to seconds
   time_t rawtime = mktime(timeinfo);
   uint32 Seconds = (uint32)rawtime;
 
-  //Set the RTC time
+  // Set the RTC time
   RTC_Set_Time(Seconds);
 }
 
@@ -89,7 +88,7 @@ void RTC_Set_Time_Format(struct tm *timeinfo)
  */
 uint32 RTC_Get_Time(void)
 {
-  return RTC_TSR;               //After counting starts, the RTC_TSR register reads the current counting time
+  return RTC_TSR; // After counting starts, the RTC_TSR register reads the current counting time
 }
 
 /*
@@ -99,9 +98,9 @@ uint32 RTC_Get_Time(void)
 struct tm RTC_Get_Time_Format(void)
 {
   struct tm *timeinfo;
-  time_t rawtime = RTC_Get_Time(); //Get the current time in seconds
-  timeinfo = localtime(&rawtime); //Convert to local time structure
-  return *timeinfo; //Return the time structure
+  time_t rawtime = RTC_Get_Time(); // Get the current time in seconds
+  timeinfo = localtime(&rawtime);  // Convert to local time structure
+  return *timeinfo;                // Return the time structure
 }
 
 /*
@@ -112,18 +111,18 @@ struct tm RTC_Get_Time_Format(void)
  */
 uint8 RTC_Set_Alarm(uint32 Alarm)
 {
-  //The alarm time cannot be less than the current initial timer time
-  if(Alarm < RTC_TSR)
+  // The alarm time cannot be less than the current initial timer time
+  if (Alarm < RTC_TSR)
   {
     return 0;
   }
-  
-  RTC_SR &= ~RTC_SR_TCE_MASK;   //Disable RTC counter
-  RTC_TAR = Alarm;              //Set Alarm Time
-  RTC_SR |= RTC_SR_TCE_MASK;    //Enable RTC counter
-  
-  RTC_IER |= RTC_IER_TAIE_MASK; //Alarm clock interrupt enable
-  
+
+  RTC_SR &= ~RTC_SR_TCE_MASK; // Disable RTC counter
+  RTC_TAR = Alarm;            // Set Alarm Time
+  RTC_SR |= RTC_SR_TCE_MASK;  // Enable RTC counter
+
+  RTC_IER |= RTC_IER_TAIE_MASK; // Alarm clock interrupt enable
+
   return 1;
 }
 
@@ -135,10 +134,10 @@ uint8 RTC_Set_Alarm(uint32 Alarm)
  */
 uint8 RTC_Set_Alarm_Format(struct tm *timeinfo)
 {
-  //Convert struct tm to seconds
+  // Convert struct tm to seconds
   time_t rawtime = mktime(timeinfo);
   uint32 Seconds = (uint32)rawtime;
-  //Set the RTC alarm time
+  // Set the RTC alarm time
   return RTC_Set_Alarm(Seconds);
 }
 
@@ -148,7 +147,7 @@ uint8 RTC_Set_Alarm_Format(struct tm *timeinfo)
  */
 void RTC_Close_Alarm(void)
 {
-  RTC_IER &= ~RTC_IER_TAIE_MASK;//Do not interrupt the alarm clock
+  RTC_IER &= ~RTC_IER_TAIE_MASK; // Do not interrupt the alarm clock
 }
 
 /*
@@ -159,29 +158,26 @@ void RTC_Close_Alarm(void)
  */
 void RTC_Test_Handler(void)
 {
-  if(RTC_SR & RTC_SR_TIF_MASK)//Invalid settings
+  if (RTC_SR & RTC_SR_TIF_MASK) // Invalid settings
   {
     RTC_SR = 0;
     RTC_TSR = 0;
-    
-    //Put Your Code...
-    
+
+    // Put Your Code...
   }
-  
-  if(RTC_SR & RTC_SR_TOF_MASK)//Count overflow
+
+  if (RTC_SR & RTC_SR_TOF_MASK) // Count overflow
   {
     RTC_SR = 0;
     RTC_TSR = 0;
-    
-    //Put Your Code...
-    
+
+    // Put Your Code...
   }
-  
-  if(RTC_SR & RTC_SR_TAF_MASK)//Alarm
+
+  if (RTC_SR & RTC_SR_TAF_MASK) // Alarm
   {
     RTC_TAR = 0;
-    
-    //Put Your Code...
+
+    // Put Your Code...
   }
-  
 }
