5c90ac04579b399eb4c3be48631171968ba033b0
Alopex6414 <1029535012@qq.com>
2026-02-27 22:58:18 +0800

    format oled_i2c.h

--- dev/inc/oled_i2c.h
+++ dev/inc/oled_i2c.h
@@ -18,69 +18,69 @@
 /*
 **I2C port definition
 */
-//I2C Port Macro
-//I2C SCL port initialization
+// I2C Port Macro
+// I2C SCL port initialization
 #ifndef OLED_I2C_PORT_INIT_SCL
-#define OLED_I2C_PORT_INIT_SCL       GPIO_Init(PTE26,GPO,0)
+#define OLED_I2C_PORT_INIT_SCL GPIO_Init(PTE26, GPO, 0)
 #endif
-//I2C SDA port initialization
+// I2C SDA port initialization
 #ifndef OLED_I2C_PORT_INIT_SDA
-#define OLED_I2C_PORT_INIT_SDA       GPIO_Init(PTE28,GPO,0)
+#define OLED_I2C_PORT_INIT_SDA GPIO_Init(PTE28, GPO, 0)
 #endif
-//I2C SCL port output
+// I2C SCL port output
 #ifndef OLED_I2C_DDR_OUT_SCL
-#define OLED_I2C_DDR_OUT_SCL         GPIO_DDRX(PTE26,1)
+#define OLED_I2C_DDR_OUT_SCL GPIO_DDRX(PTE26, 1)
 #endif
-//I2C SDA port output
+// I2C SDA port output
 #ifndef OLED_I2C_DDR_OUT_SDA
-#define OLED_I2C_DDR_OUT_SDA         GPIO_DDRX(PTE28,1)
+#define OLED_I2C_DDR_OUT_SDA GPIO_DDRX(PTE28, 1)
 #endif
-//I2C SDA port input
+// I2C SDA port input
 #ifndef OLED_I2C_DDR_IN_SDA
-#define OLED_I2C_DDR_IN_SDA          GPIO_DDRX(PTE28,0)
+#define OLED_I2C_DDR_IN_SDA GPIO_DDRX(PTE28, 0)
 #endif
-//I2C SCL port high level
+// I2C SCL port high level
 #ifndef OLED_I2C_SET_SCL_H
-#define OLED_I2C_SET_SCL_H           GPIO_SET(PTE26,1)
+#define OLED_I2C_SET_SCL_H GPIO_SET(PTE26, 1)
 #endif
-//I2C SCL port low level
+// I2C SCL port low level
 #ifndef OLED_I2C_SET_SCL_L
-#define OLED_I2C_SET_SCL_L           GPIO_SET(PTE26,0)
+#define OLED_I2C_SET_SCL_L GPIO_SET(PTE26, 0)
 #endif
-//I2C SDA port high level
+// I2C SDA port high level
 #ifndef OLED_I2C_SET_SDA_H
-#define OLED_I2C_SET_SDA_H           GPIO_SET(PTE28,1)
+#define OLED_I2C_SET_SDA_H GPIO_SET(PTE28, 1)
 #endif
-//I2C SDA port low level
+// I2C SDA port low level
 #ifndef OLED_I2C_SET_SDA_L
-#define OLED_I2C_SET_SDA_L           GPIO_SET(PTE28,0)
+#define OLED_I2C_SET_SDA_L GPIO_SET(PTE28, 0)
 #endif
-//I2C SDA port input signal
+// I2C SDA port input signal
 #ifndef OLED_I2C_GET_SDA_IN
-#define OLED_I2C_GET_SDA_IN          GPIO_GET(PTE28)
+#define OLED_I2C_GET_SDA_IN GPIO_GET(PTE28)
 #endif
 
-#define OLED_I2C_DELAY_TIME          I2C_Delay(10)
+#define OLED_I2C_DELAY_TIME I2C_Delay(10)
 
 /*
 **Oled coordinate definition
 */
-#define OLED_I2C_XLevelL                0x00                            //OledX axis low position
-#define OLED_I2C_XLevelH                0x10                            //OledX axis high position
-#define OLED_I2C_XLevel	        ((XLevelH&0x0F)*16+XLevelL)             //OledX axis coordinate
-#define OLED_I2C_Max_COL	            128                             //Oled maximum number of columns
-#define OLED_I2C_Max_ROW		        64                              //Oled maximum number of rows
-#define OLED_I2C_Max_Page               8                               //Oled maximum number of pages
-#define	OLED_I2C_BRIGHTNESS	            0xCF                            //Oled brightness
-#define OLED_I2C_X_WIDTH                128                             //OledX axis width
-#define OLED_I2C_Y_WIDTH                64                              //OledY axis width
+#define OLED_I2C_XLevelL 0x00                             // OledX axis low position
+#define OLED_I2C_XLevelH 0x10                             // OledX axis high position
+#define OLED_I2C_XLevel ((XLevelH & 0x0F) * 16 + XLevelL) // OledX axis coordinate
+#define OLED_I2C_Max_COL 128                              // Oled maximum number of columns
+#define OLED_I2C_Max_ROW 64                               // Oled maximum number of rows
+#define OLED_I2C_Max_Page 8                               // Oled maximum number of pages
+#define OLED_I2C_BRIGHTNESS 0xCF                          // Oled brightness
+#define OLED_I2C_X_WIDTH 128                              // OledX axis width
+#define OLED_I2C_Y_WIDTH 64                               // OledY axis width
 
-//Oled printing color enumeration
+// Oled printing color enumeration
 typedef enum
 {
-  OLED_Pure_Color,       //Orthochromatic
-  OLED_Invert_Color,     //Reverse color
-}OLED_Color_e;
+  OLED_Pure_Color,   // Orthochromatic
+  OLED_Invert_Color, // Reverse color
+} OLED_Color_e;
 
 /*
 **variate declaration
@@ -108,35 +108,35 @@ extern const uint8 LCM_StopWatch_Digit[];
 */
 extern void Oled_I2C_WrDat(uint8 data);
 extern void Oled_I2C_WrCmd(uint8 cmd);
-extern void Oled_I2C_Set_Pos(uint8 x,uint8 y);
+extern void Oled_I2C_Set_Pos(uint8 x, uint8 y);
 extern void Oled_I2C_Fill(uint8 data);
 extern void Oled_I2C_Clean(void);
 extern void Oled_I2C_Reset(void);
 extern void Oled_I2C_Init(void);
-extern void Oled_I2C_Put_Pixel(uint8 x,uint8 y);
-extern void Oled_I2C_Draw_BMP_128x64(const uint8 *Buff,OLED_Color_e Oled_Color_x);
-extern void Oled_I2C_Put_Str_6x8(uint8 x,uint8 y,uint8 ch[]);
-extern void Oled_I2C_Put_Str_8x16(uint8 x,uint8 y,uint8 ch[]);
-extern void Oled_I2C_Put_Str_12x24(uint8 x,uint8 y,uint8 ch[]);
+extern void Oled_I2C_Put_Pixel(uint8 x, uint8 y);
+extern void Oled_I2C_Draw_BMP_128x64(const uint8 *Buff, OLED_Color_e Oled_Color_x);
+extern void Oled_I2C_Put_Str_6x8(uint8 x, uint8 y, uint8 ch[]);
+extern void Oled_I2C_Put_Str_8x16(uint8 x, uint8 y, uint8 ch[]);
+extern void Oled_I2C_Put_Str_12x24(uint8 x, uint8 y, uint8 ch[]);
 extern void Oled_I2C_Draw_Picture_128x64(const uint8 *Picture);
-extern void Oled_I2C_Put_Para_6x8(uint8 x,uint8 y,uint8 ch[],int Value);
-extern void Oled_I2C_Put_Para_Pot_6x8(uint8 x,uint8 y,uint8 ch[],float Value);
-extern void Oled_I2C_Put_Para_8x16(uint8 x,uint8 y,uint8 ch[],int Value);
-extern void Oled_I2C_Put_Para_Pot_8x16(uint8 x,uint8 y,uint8 ch[],float Value);
+extern void Oled_I2C_Put_Para_6x8(uint8 x, uint8 y, uint8 ch[], int Value);
+extern void Oled_I2C_Put_Para_Pot_6x8(uint8 x, uint8 y, uint8 ch[], float Value);
+extern void Oled_I2C_Put_Para_8x16(uint8 x, uint8 y, uint8 ch[], int Value);
+extern void Oled_I2C_Put_Para_Pot_8x16(uint8 x, uint8 y, uint8 ch[], float Value);
 
-extern void Oled_I2C_Put_Time_6x8_12(uint8 x,uint8 y,uint8 ch[],int hour,int minute,int second, int isAM);
-extern void Oled_I2C_Put_Time_Wo_Sec_6x8_12(uint8 x,uint8 y,uint8 ch[],int hour,int minute,int isAM);
-extern void Oled_I2C_Put_Time_6x8_24(uint8 x,uint8 y,uint8 ch[],int hour,int minute,int second);
-extern void Oled_I2C_Put_Time_Wo_Sec_6x8_24(uint8 x,uint8 y,uint8 ch[],int hour,int minute);
-extern void Oled_I2C_Put_Time_8x16_12(uint8 x,uint8 y,uint8 ch[],int hour,int minute,int second,int isAM);
-extern void Oled_I2C_Put_Time_Wo_Sec_8x16_12(uint8 x,uint8 y,uint8 ch[],int hour,int minute,int isAM);
-extern void Oled_I2C_Put_Time_8x16_24(uint8 x,uint8 y,uint8 ch[],int hour,int minute,int second);
-extern void Oled_I2C_Put_Time_Wo_Sec_8x16_24(uint8 x,uint8 y,uint8 ch[],int hour,int minute);
-extern void Oled_I2C_Put_Time_12x24_12(uint8 x,uint8 y,uint8 ch[],int hour,int minute,int second,int isAM);
-extern void Oled_I2C_Put_Time_Wo_Sec_12x24_12(uint8 x,uint8 y,uint8 ch[],int hour,int minute,int isAM);
-extern void Oled_I2C_Put_Time_12x24_24(uint8 x,uint8 y,uint8 ch[],int hour,int minute,int second);
-extern void Oled_I2C_Put_Time_Wo_Sec_12x24_24(uint8 x,uint8 y,uint8 ch[],int hour,int minute);
-extern void Oled_I2C_Put_Time_Wo_Sec_12x24_24_Cursor(uint8 x,uint8 y,uint8 ch[],int hour,int minute,int cursor);
-extern void Oled_I2C_Put_Date_6x8(uint8 x,uint8 y,uint8 ch[],int year,int month,int day);
+extern void Oled_I2C_Put_Time_6x8_12(uint8 x, uint8 y, uint8 ch[], int hour, int minute, int second, int isAM);
+extern void Oled_I2C_Put_Time_Wo_Sec_6x8_12(uint8 x, uint8 y, uint8 ch[], int hour, int minute, int isAM);
+extern void Oled_I2C_Put_Time_6x8_24(uint8 x, uint8 y, uint8 ch[], int hour, int minute, int second);
+extern void Oled_I2C_Put_Time_Wo_Sec_6x8_24(uint8 x, uint8 y, uint8 ch[], int hour, int minute);
+extern void Oled_I2C_Put_Time_8x16_12(uint8 x, uint8 y, uint8 ch[], int hour, int minute, int second, int isAM);
+extern void Oled_I2C_Put_Time_Wo_Sec_8x16_12(uint8 x, uint8 y, uint8 ch[], int hour, int minute, int isAM);
+extern void Oled_I2C_Put_Time_8x16_24(uint8 x, uint8 y, uint8 ch[], int hour, int minute, int second);
+extern void Oled_I2C_Put_Time_Wo_Sec_8x16_24(uint8 x, uint8 y, uint8 ch[], int hour, int minute);
+extern void Oled_I2C_Put_Time_12x24_12(uint8 x, uint8 y, uint8 ch[], int hour, int minute, int second, int isAM);
+extern void Oled_I2C_Put_Time_Wo_Sec_12x24_12(uint8 x, uint8 y, uint8 ch[], int hour, int minute, int isAM);
+extern void Oled_I2C_Put_Time_12x24_24(uint8 x, uint8 y, uint8 ch[], int hour, int minute, int second);
+extern void Oled_I2C_Put_Time_Wo_Sec_12x24_24(uint8 x, uint8 y, uint8 ch[], int hour, int minute);
+extern void Oled_I2C_Put_Time_Wo_Sec_12x24_24_Cursor(uint8 x, uint8 y, uint8 ch[], int hour, int minute, int cursor);
+extern void Oled_I2C_Put_Date_6x8(uint8 x, uint8 y, uint8 ch[], int year, int month, int day);
 
 #endif
